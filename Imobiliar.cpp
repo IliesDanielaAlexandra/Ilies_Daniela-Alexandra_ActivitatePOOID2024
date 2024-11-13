072d6959cdfa57194b8a67ba792bb98eb7d5ac53
iliesdaniela22 <iliesdaniela22@stud.ase.ro>
2024-11-13 23:27:10 +0100

    13 11 2024

--- Imobiliar.cpp
+++ Imobiliar.cpp
@@ -2,6 +2,9 @@
 #include<string>
 using namespace std;
 
+/*Alegeți trei obiecte din realitate. Cele trei obiecte trebuie să fie din același domeniu. Domeniul îl
+alegeți astfel încât să conțină inițialele voastre. */
+
 /*Domeniul Imobiriar */
 
 class Bloc {
@@ -9,16 +12,16 @@ public:
 	static int idBloc;
 	int nrApartamente;
 	char* dezvoltator;
-	const int anConstructie = 1960;
+	const int anConstructie;
 
-	Bloc() {
+	Bloc() : dezvoltator(nullptr), anConstructie(1960) {
+		idBloc++;
 		this->nrApartamente = 0;
 		this->dezvoltator = nullptr;
-		idBloc = idBloc + 1;
-
 	}
 
-	Bloc(const char* dezvoltator) :anConstructie(anConstructie) {
+	Bloc(const char* dezvoltator) :dezvoltator(nullptr), anConstructie(1960) {
+		idBloc++;
 		this->nrApartamente = 0;
 		if (dezvoltator != nullptr) {
 			this->dezvoltator = new char[strlen(dezvoltator) + 1];
@@ -29,12 +32,12 @@ public:
 
 		}
 
-		idBloc = idBloc + 1;
+
 	}
 
-	Bloc(int nrApartamente, const char* dezvoltator) :anConstructie(anConstructie) {
+	Bloc(int nrApartamente, const char* dezvoltator) :dezvoltator(nullptr), anConstructie(1960) {
 
-		idBloc = idBloc + 1;
+		idBloc++;
 		this->nrApartamente = nrApartamente;
 		if (dezvoltator != nullptr)
 		{
@@ -53,6 +56,20 @@ public:
 		delete[] dezvoltator;
 	}
 
+	void afisareBloc() {
+		cout << "Id Bloc=" << idBloc << endl;
+		cout << "Numar apartamente= " << nrApartamente << endl;
+		if (dezvoltator != nullptr) {
+			cout << "Dezvoltator = " << dezvoltator << endl;
+		}
+		else
+		{
+			cout << " Dezvoltatorul nu este atribuit" << endl;
+		}
+
+		cout << "An Constructie =" << anConstructie << endl << endl;
+
+	}
 };
 
 class Dezvoltator {
@@ -61,9 +78,9 @@ public:
 	string numeDezvoltator;
 	int nrAngajati;
 	float* salariiAngajati;
-	const float tva = 0.24;
+	const float tva;
 
-	Dezvoltator() :tva(tva) {
+	Dezvoltator() :tva(0.24) {
 
 		idDezvoltator = idDezvoltator + 1;
 		this->numeDezvoltator = "";
@@ -71,14 +88,14 @@ public:
 		this->salariiAngajati = nullptr;
 
 	}
-	Dezvoltator(string numeDezvoltator, int nrAngajati) :tva(tva) {
+	Dezvoltator(string numeDezvoltator, int nrAngajati) :tva(0.24) {
 		idDezvoltator = idDezvoltator + 1;
 		this->numeDezvoltator = numeDezvoltator;
 		this->nrAngajati = nrAngajati;
 		this->salariiAngajati = nullptr;
 	}
 
-	Dezvoltator(string numeDezvoltator, int nrAngajati, float* salariiAngajati) : tva(tva)
+	Dezvoltator(string numeDezvoltator, int nrAngajati, float* salariiAngajati) : tva(0.24)
 	{
 		idDezvoltator = idDezvoltator + 1;
 		this->numeDezvoltator = numeDezvoltator;
@@ -98,6 +115,31 @@ public:
 		delete[] this->salariiAngajati;
 	}
 
+	void afisareDezvoltator() {
+		cout << "Id Dezvoltatot=" << idDezvoltator << endl;
+		cout << "Nume Dezvoltator=" << numeDezvoltator << endl;
+		cout << "Numar Angajati=" << nrAngajati << endl;
+		if (salariiAngajati != nullptr)
+		{
+			if (nrAngajati != 0) {
+				for (int i = 0; i < nrAngajati; i++)
+				{
+
+					cout << "Salariul angajatului " << i + 1 << " este =" << salariiAngajati[i] << endl;
+				}
+			}
+			else
+			{
+				cout << "Nu exista angajati deci nu exista salarii! " << endl;
+			}
+		}
+		else
+		{
+			cout << "Nu avem salarii definite!" << endl;
+		}
+
+		cout << " TVA =" << tva << endl << endl;
+	}
 };
 
 class Apartament {
@@ -161,17 +203,43 @@ int Apartament::idApartament = 0;
 void main() {
 
 	Bloc b1;
+	b1.afisareBloc();
+	//cout << "b1" << endl;
+	//cout << b1.anConstructie << endl;
+
+	//cout << b1.idBloc << endl;
+	//cout << b1.nrApartamente << endl;
+
 	Bloc b2("Dezvoltator1");
+	b2.afisareBloc();
+
+	//cout <<"b2" << endl;
+	//cout << b2.anConstructie << endl;
+	//cout << b2.dezvoltator << endl;
+	//cout << b2.idBloc << endl;
+	//cout << b2.nrApartamente << endl;
+
 	Bloc b3(10, "Dezvoltator2");
+	b3.afisareBloc();
+	/*cout << "b3" << endl;
+	cout << b3.anConstructie << endl;
+	cout << b3.dezvoltator << endl;
+	cout << b3.idBloc << endl;
+	cout << b3.nrApartamente << endl;*/
+
 
 	Dezvoltator D1;
+	D1.afisareDezvoltator();
 	Dezvoltator D2("Dezvoltator1", 10);
+	D2.afisareDezvoltator();
 	float salrii[5] = { 5500,6500,4900,7300,8000 };
 	Dezvoltator D3("Dezvoltator 2", 5, salrii);
+	D3.afisareDezvoltator();
 
 	Apartament A1;
 	Apartament A2(3, 550000);
 	float suprafata[4] = { 12.2 , 13, 15.5 ,14 };
 	Apartament A3(4, suprafata, 800000, "Bloc1");
+
 
 }
